9e609b4ffb4afc315dc565776999e38b924d23af
AetherPhx <aetherphx@gmail.com>
2024-09-23 18:51:10 -0500

    Generar estructura de componente Card del SliderGallery

---
+++ src/components/specific/sliderGallery/sliderGalleryCard/SliderGalleryCard.jsx
@@ -0,0 +1,12 @@
+export const SliderGalleryCard = () => {
+	// TODO: Agregar los siguientes props:
+	// 	imageUrl, name, discountPercent, currentPrice, originalPrice
+
+	return (
+		<article>
+			<img src="" alt="Imagen" />
+			<h3>Titulo</h3>
+			<button>Texto de Botón</button>
+		</article>
+	);
+};
