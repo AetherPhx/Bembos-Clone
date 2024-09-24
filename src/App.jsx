a696277591e51b3145058a5f42f76ca1b0442553
AetherPhx <aetherphx@gmail.com>
2024-09-24 17:49:57 -0500

    Preparar entorno para trabajar con el Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import { Header } from "./components/common/header";
 import { Home } from "./pages/home";
-/*import { Footer } from "./components/common/footer";*/
+import { Footer } from "./components/common/footer";
 
 export function App() {
 	return (
-
 		<>
-			<h1>Hello World</h1>
 			{/* <Header /> */}
 			{/* <Home /> */}
 			<Footer />
-			
-		
-
-
 		</>
-
-	
-		
 	);
 }
