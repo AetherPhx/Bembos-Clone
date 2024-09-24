14652b5f8abcc027c28e6ce508a6dd24b4ff2e95
AetherPhx <aetherphx@gmail.com>
2024-09-24 15:11:31 -0500

    Desarrollar tarjeta de SliderGallery

--- src/components/specific/sliderGallery/sliderGalleryCard/SliderGalleryCard.jsx
+++ src/components/specific/sliderGallery/sliderGalleryCard/SliderGalleryCard.jsx
@@ -1,12 +1,19 @@
+import "./sliderGalleryCard.css";
 export const SliderGalleryCard = () => {
 	// TODO: Agregar los siguientes props:
 	// 	imageUrl, name, discountPercent, currentPrice, originalPrice
 
 	return (
-		<article>
-			<img src="" alt="Imagen" />
-			<h3>Titulo</h3>
-			<button>Texto de Botón</button>
+		<article className="SliderGalleryCard">
+			<div className="SliderGalleryCard-imageWrapper">
+				<img
+					className="SliderGalleryCard-image"
+					src="https://www.bembos.com.pe/_ipx/q_60,s_275x368/https://d31npzejelj8v1.cloudfront.net/media/catalog/category/promociones-bembos-delivery.webp"
+					alt="Imagen"
+				/>
+			</div>
+			<h3 className="SliderGalleryCard-title">Titulo</h3>
+			<button className="SliderGalleryCard-button">Texto de Botón</button>
 		</article>
 	);
 };
