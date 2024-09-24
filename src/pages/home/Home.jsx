33e6c283f77542577d7b0505c5414f87eda67950
AetherPhx <aetherphx@gmail.com>
2024-09-24 15:10:24 -0500

    Agregar espacio seguro al home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,13 @@
 import { Banner } from "/src/components/specific/banner";
 import { SliderGallery } from "/src/components/specific/sliderGallery";
+import "./home.css";
 
 export const Home = () => {
 	// TODO: Agregar banner únicamente con la primera imágen.
 	// TODO: Desarrollar SliderGallery para "Promociones y Menú de Hamburguesas Online"
 
 	return (
-		<main>
+		<main className="Home">
 			{/* <Banner /> */}
 
 			{/* <SliderGallery /> */}
