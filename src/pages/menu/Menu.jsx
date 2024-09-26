5ae9d8825c2632d87c854632ca2124b72800543c
AetherPhx <aetherphx@gmail.com>
2024-09-26 13:09:36 -0500

    Testear que se puede obtener la información de cada categoría

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,11 +1,45 @@
 import { useData } from "/src/context/ReactContext";
 
 export const Menu = () => {
-	const { mostrarProds } = useData();
+	const {
+		combos,
+		hamburguesas,
+		pollo,
+		loncheritas,
+		complementos,
+		bebidas,
+		helados,
+		inkaChips,
+		menu,
+		promoOnline,
+		promoPersonales,
+		promoDos,
+		promoCompartir,
+		cupones,
+		locales,
+	} = useData();
+
+	const mostrarCombos = () => {
+		console.log(combos);
+		console.log(hamburguesas);
+		console.log(pollo);
+		console.log(loncheritas);
+		console.log(complementos);
+		console.log(bebidas);
+		console.log(helados);
+		console.log(inkaChips);
+		console.log(menu);
+		console.log(promoOnline);
+		console.log(promoPersonales);
+		console.log(promoDos);
+		console.log(promoCompartir);
+		console.log(cupones);
+		console.log(locales);
+	};
 
 	return (
 		<>
-			<button onClick={mostrarProds}>Mostrar</button>
+			<button onClick={mostrarCombos}>Menu</button>
 		</>
 	);
 };
