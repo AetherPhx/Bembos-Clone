54bf2f1474200d87e4c77b78b6d0548162c4267c
AetherPhx <aetherphx@gmail.com>
2024-09-26 13:09:07 -0500

    Realizar peticiones desde el ReactContext.jsx para tener la información de la db por categorías

--- src/context/ReactContext.jsx
+++ src/context/ReactContext.jsx
@@ -1,103 +1,110 @@
 import { createContext, useState, useContext, useEffect } from "react";
+import axios from "axios";
 
-// const UserContext = createContext();
 const DataContext = createContext();
+const BASE_URL = "http://localhost:3000/";
 
 export const useData = () => useContext(DataContext);
 
 export const DataProvider = ({ children }) => {
-	const [combos, setCombos] = useState([]);
-	const [loncheritas, setLoncheritas] = useState([]);
+	const [combos, setCombos] = useState([]),
+		[hamburguesas, setHamburguesas] = useState([]),
+		[pollo, setPollo] = useState([]),
+		[loncheritas, setLoncheritas] = useState([]),
+		[complementos, setComplementos] = useState([]),
+		[bebidas, setBebidas] = useState([]),
+		[helados, setHelados] = useState([]),
+		[inkaChips, setInkaChips] = useState([]),
+		[menu, setMenu] = useState([]),
+		[promoOnline, setPromoOnline] = useState([]),
+		[promoPersonales, setPromoPersonales] = useState([]),
+		[promoDos, setPromoDos] = useState([]),
+		[promoCompartir, setPromoCompartir] = useState([]),
+		[cupones, setCupones] = useState([]),
+		[locales, setLocales] = useState([]);
 
 	useEffect(() => {
-		setCombos([
-			{
-				nombre: "Combo Extrema",
-				precio: 32.8,
-				id: 1,
-				img: "https://www.bembos.com.pe/_ipx/q_85,w_275,f_webp/https://d31npzejelj8v1.cloudfront.net/media/catalog/product/8/0/800x1370-combo-extrema-nuevo-bb.jpg",
-				link: "combo-extrema",
-			},
-			{
-				nombre: "Combo Carretillera",
-				precio: 30.8,
-				id: 2,
-				img: "https://www.bembos.com.pe/_ipx/q_85,w_275,f_webp/https://d31npzejelj8v1.cloudfront.net/media/catalog/product/8/0/800x1370-combo-carretillera-nuevo-bb.jpg",
-				link: "combo-carretillera",
-			},
-		]);
-		setLoncheritas([
-			{
-				tipos: [
-					{
-						nombre: "Loncherita básica de pollo",
-						precio: 10.9,
-						id: 26,
-						img: "https://www.bembos.com.pe/_ipx/q_85,w_144,f_webp/https://d31npzejelj8v1.cloudfront.net/media/catalog/product/l/o/loncherita-basica-pollo.jpg",
-						link: "loncherita-basica-de-pollo",
-					},
-					{
-						nombre: "Loncherita Básica",
-						precio: 10.9,
-						id: 27,
-						img: "https://www.bembos.com.pe/_ipx/q_85,w_144,f_webp/https://d31npzejelj8v1.cloudfront.net/media/catalog/product/l/o/loncherita-basica_1.jpg",
-						link: "loncherita-basica",
-					},
-				],
-			},
-			{
-				preguntas: [
-					{
-						nropregunta: 2,
-						nombrepregunta: "ELIGE EL TAMAÑO DE TU PAPA",
-						descrippregunta: "",
-						combinaciones: [
-							{
-								nrocomb: 11,
-								titulocomb: "Papa regular + S/.0.00",
-								imgcomb:
-									"https://www.bembos.com.pe/_ipx/q_60,s_72x72/https://d31npzejelj8v1.cloudfront.net/media/catalog/product/8/0/800x800-papas-reg_2.webp",
-								preccomb: 0,
-							},
-							{
-								nrocomb: 12,
-								titulocomb: "Papa Mediana + S/.1.00",
-								imgcomb:
-									"https://www.bembos.com.pe/_ipx/q_60,s_72x72/https://d31npzejelj8v1.cloudfront.net/media/catalog/product/p/a/papas_grandes_1_1_2.webp",
-								preccomb: 1,
-							},
-							{
-								nrocomb: 13,
-								titulocomb: "Papa Grande + S/.2.00",
-								imgcomb:
-									"https://www.bembos.com.pe/_ipx/q_60,s_72x72/https://d31npzejelj8v1.cloudfront.net/media/catalog/product/p/a/papas_grandes_2_1.webp",
-								preccomb: 2,
-							},
-						],
-					},
-				],
-			},
-		]);
+		const requestData = async () => {
+			try {
+				Promise.all([
+					axios.get(BASE_URL + "combos"),
+					axios.get(BASE_URL + "hamburguesas"),
+					axios.get(BASE_URL + "pollo"),
+					axios.get(BASE_URL + "loncheritas"),
+					axios.get(BASE_URL + "complementos"),
+					axios.get(BASE_URL + "bebidas"),
+					axios.get(BASE_URL + "helados"),
+					axios.get(BASE_URL + "inka-chips"),
+					axios.get(BASE_URL + "menu"),
+					axios.get(BASE_URL + "promociones_online"),
+					axios.get(BASE_URL + "promociones_personales"),
+					axios.get(BASE_URL + "promociones_para_dos"),
+					axios.get(BASE_URL + "promociones_para_compartir"),
+					axios.get(BASE_URL + "cupones"),
+					axios.get(BASE_URL + "locales"),
+				]).then(
+					([
+						combos,
+						hamburguesas,
+						pollo,
+						loncheritas,
+						complementos,
+						bebidas,
+						helados,
+						inkaChips,
+						menu,
+						promoOnline,
+						promoPersonales,
+						promoDos,
+						promoCompartir,
+						cupones,
+						locales,
+					]) => {
+						setCombos(combos.data);
+						setHamburguesas(hamburguesas.data);
+						setPollo(pollo.data);
+						setLoncheritas(loncheritas.data);
+						setComplementos(complementos.data);
+						setBebidas(bebidas.data);
+						setHelados(helados.data);
+						setInkaChips(inkaChips.data);
+						setMenu(menu.data);
+						setPromoOnline(promoOnline.data);
+						setPromoPersonales(promoPersonales.data);
+						setPromoDos(promoDos.data);
+						setPromoCompartir(promoCompartir.data);
+						setCupones(cupones.data);
+						setLocales(locales.data);
+					}
+				);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		requestData();
 	}, []);
 
-	const mostrarProds = () => {
-		console.log(combos);
-		console.log(loncheritas);
-	};
-
-	// const cambiarUsuario = () => {
-	// 	if (user) {
-	// 		setUser(null);
-	// 	} else {
-	// 		setUser({
-	// 			nombre: "Luis",
-	// 			apellido: "Mendoza",
-	// 		});
-	// 	}
-	// };
-
 	return (
-		<DataContext.Provider value={{ combos, loncheritas, mostrarProds }}>
+		<DataContext.Provider
+			value={{
+				combos,
+				hamburguesas,
+				pollo,
+				loncheritas,
+				complementos,
+				bebidas,
+				helados,
+				inkaChips,
+				menu,
+				promoOnline,
+				promoPersonales,
+				promoDos,
+				promoCompartir,
+				cupones,
+				locales,
+			}}
+		>
 			{children}
 		</DataContext.Provider>
 	);
