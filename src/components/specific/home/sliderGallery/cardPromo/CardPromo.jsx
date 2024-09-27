a26ac0d541247a0eac1c242d1f98c58d6541fccc
AetherPhx <aetherphx@gmail.com>
2024-09-26 19:10:41 -0500

    Preparar presentación

--- src/components/specific/home/sliderGallery/cardPromo/CardPromo.jsx
+++ src/components/specific/home/sliderGallery/cardPromo/CardPromo.jsx
@@ -20,11 +20,17 @@ export const CardPromo = ({
 					alt="Heart"
 				/>
 			</header>
+
 			<div className="CardPromo-imageWrapper">
 				<img className="CardPromo-image" src={imageUrl} alt="Imagen" />
 			</div>
+
 			<div className="CardPromo-info">
 				<h3 className="Font-title-middle CardPromo-title">{name}</h3>
+				<div className="CardPromo-cost">
+					<p>S/. {currentPrice}</p>
+					<p>S/. {previousPrice}</p>
+				</div>
 				<button className="Font-button-primary CardPromo-button">
 					{btnText}
 				</button>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,50 +1,49 @@
+import { useData } from "/src/context/ReactContext";
 import { Header, Footer } from "/src/components/common";
 import { Slider } from "/src/components/specific/home/slider";
-import { useData } from "/src/context/ReactContext";
-// import { Banner } from "/src/components/specific/home/banner";
-
 import { SliderGallery } from "/src/components/specific/home/sliderGallery";
 import { BannerCard } from "/src/components/specific/home/bannerCard";
 import { SocialBanner } from "../../components/specific/home/socialBanner/SocialBanner";
 import "./home.css";
 
 export const Home = () => {
-	// TODO: Agregar banner únicamente con la primera imágen.
-	// TODO: Desarrollar SliderGallery para "Promociones y Menú de Hamburguesas Online"
-
 	const { promoOnline, menu } = useData();
+	const newPromoOnline = [];
+	const newMenu = [];
+	for (let index = 0; index < 4; index++) {
+		newPromoOnline.push(promoOnline[index]);
+	}
+	for (let index = 0; index < 4; index++) {
+		newMenu.push(menu[index]);
+	}
 
 	return (
 		<>
 			<Header />
 
-			<main >
+			<main>
 				<Slider />
-    
+
 				<div className="Home">
-					<SliderGallery title="MENÚ DE HAMBURGUESAS ONLINE" />
+					<SliderGallery
+						title="PROMOCIONES DE HAMBURGUESAS ONLINE"
+						data={newPromoOnline}
+						cardType="Promo"
+					/>
 
-				<SliderGallery
-					title="PROMOCIONES DE HAMBURGUESAS ONLINE"
-					data={promoOnline}
-					cardType="Promo"
-				/>
+					<SliderGallery
+						title="MENÚ DE HAMBURGUESAS ONLINE"
+						data={newMenu}
+						cardType="Simple"
+					/>
 
-				<SliderGallery
-					title="MENÚ DE HAMBURGUESAS ONLINE"
-					data={menu}
-					cardType="Simple"
-				/>
-            
-            <section className="Home-promos">
+					<section className="Home-promos">
 						<BannerCard url="https://d31npzejelj8v1.cloudfront.net/media/promotionspage/images/promo-store/default/1719412352_Banner-Delivery-home-left.webp" />
 						<BannerCard url="https://d31npzejelj8v1.cloudfront.net/media/promotionspage/images/promo-store-mobile/default/1719412352_new-banner-cupones.webp" />
 					</section>
 
-
 					<SocialBanner />
 				</div>
-				
 			</main>
 
 			<Footer />
