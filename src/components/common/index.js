61ba3b12fa0dbb3af773d10b4f4f92db14d93323
AetherPhx <aetherphx@gmail.com>
2024-09-30 17:40:54 -0500

    Eliminar del indice

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,6 +1,5 @@
 export { Header } from "./header";
 export { Footer } from "./footer";
 export { Cart } from "./cart";
-export { EmptyCart } from "./emptyCart";
 export { SecondNav } from "./secondNav";
 export { DetailFooter } from "./detailFooter";
