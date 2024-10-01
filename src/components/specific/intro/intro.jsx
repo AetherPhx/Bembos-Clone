6f9b1705f894531b719b57ac93ad10342e634df0
Victor Trigos <victor.oeaide@gmail.com>
2024-10-01 12:19:50 -0500

    Actualizar componente Intro con props

--- src/components/specific/intro/intro.jsx
+++ src/components/specific/intro/intro.jsx
@@ -1,9 +1,13 @@
 import "./Intro.css";
 
-export const Intro = () => {
+export const Intro = ({ title, description, question }) => {
     return (
         <div className="Intro-content">
-            <div className="Intro-wrapper">Intro</div>
+            <div className="Intro-wrapper">
+                <h2 className="Intro-Font-title">{title}</h2>
+                <p className="Intro-description">{description}</p>
+                <h3 className="Intro-title-questions">PREGUNTAS FRECUENTES {question}</h3>
+            </div>
         </div>
     )
 }
