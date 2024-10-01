fb2ae63b24685c11b4e492417d19b0f84de1d387
AetherPhx <aetherphx@gmail.com>
2024-10-01 18:23:23 -0500

    Preparar información que se manejará en CartContext

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,91 +1,72 @@
 const item = {
 	// Properties
-			id:"1",
-			nombre: "familiar1",
-			precioTotal:0.0,
-			cantidad:1,
-			precioUnitario:49.9,
-			detalles:"4 royal regulares + 4 papas medianas",
-			imagen:"",
-
-	// ! Agrega todas las propiedades
+	id: "1",
+	nombre: "familiar1",
+	precioTotal: 0.0,
+	cantidad: 2,
+	precioUnitario: 10.0,
+	detalles: "4 royal regulares + 4 papas medianas",
+	imagen: "",
 
 	// Methods
 	increase() {
-    this.cantidad += 1;
-  },
-      
-    reduce() {
-
-        if (this.cantidad > 1) {
-            this.cantidad -= 1;
-        }
-    }
-  
-  calculateTotal(){
-		this.precioTotal=this.precioUnitario*this.cantidad;
+		this.cantidad += 1;
 	},
 
+	reduce() {
+		if (this.cantidad > 1) {
+			this.cantidad -= 1;
+		}
+	},
+
+	calculateTotal() {
+		this.precioTotal = this.precioUnitario * this.cantidad;
+	},
 };
 
 const cartData = {
+	total: 0,
+
 	productList: [
 		{
-			id:"1",
+			id: "1",
 			nombre: "familiar1",
-			precioTotal:0,
-			cantidad:1,
-			precioUnitario:49.9,
-			detalles:"4 royal regulares + 4 papas medianas",
-			imagen:"",
-			// Más propiedades
+			precioTotal: 0,
+			cantidad: 1,
+			precioUnitario: 49.9,
+			detalles: "4 royal regulares + 4 papas medianas",
+			imagen: "",
 		},
 		{
-			id:"2",
+			id: "2",
 			nombre: "Bembosboster clasica",
-			precioTotal:0,
-			cantidad:1,
-			precioUnitario:12.9,
-			detalles:"1 hamburguesa broster",
-			imagen:"",
-			// Más propiedades
+			precioTotal: 0,
+			cantidad: 1,
+			precioUnitario: 12.9,
+			detalles: "1 hamburguesa broster",
+			imagen: "",
 		},
 		{
-			id:"3",
+			id: "3",
 			nombre: "Bembosboster",
-			precioTotal:0,
-			cantidad:1,
-			precioUnitario:15.9,
-			detalles:"1 hamburguesa broster",
-			imagen:"",
-			// Más propiedades
+			precioTotal: 0,
+			cantidad: 1,
+			precioUnitario: 15.9,
+			detalles: "1 hamburguesa broster",
+			imagen: "",
 		},
-
-		// Más productos
 	],
 
-//calcular el total
-
-	calcTotal: () => {
-		let totalcarrito=0;
-
-		cartData.productList.forEach(product =>{
-			product.precioTotal=product.cantidad * product.precioUnitario;
-			totalcarrito+=product.precioTotal;
-		})
-			return totalcarrito;
+	calcularTotal: () => {
+		this.productList.forEach((product) => {
+			product.calculateTotal;
+			this.total += product.precioTotal;
+		});
 	},
-	//const total=calcularTotal();
-	//console.log("total",total);
 
-//eliminar un item
 	eliminarProducto: (id) => {
-		cartData.productList = cartData.productList.filter(product => product.id !== id);	
-
+		cartData.productList = cartData.productList.filter(
+			(product) => product.id !== id
+		);
 	},
-	// Eliminar el producto con id "2"
-	//eliminarProducto("2");
-	//console.log(cartData.productList);
-
-
 };
