3a1ddb5ca31e83b07366c07842e10e7e148cc609
AetherPhx <aetherphx@gmail.com>
2024-10-02 19:03:19 -0500

    Desarrollar contexto de carrito

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,72 +1,87 @@
-const item = {
-	// Properties
-	id: "1",
-	nombre: "familiar1",
-	precioTotal: 0.0,
-	cantidad: 2,
-	precioUnitario: 10.0,
-	detalles: "4 royal regulares + 4 papas medianas",
-	imagen: "",
+import { createContext, useState, useContext, useEffect } from "react";
 
-	// Methods
-	increase() {
-		this.cantidad += 1;
-	},
+const CartContext = createContext();
 
-	reduce() {
-		if (this.cantidad > 1) {
-			this.cantidad -= 1;
-		}
-	},
+export const useCart = () => useContext(CartContext);
 
-	calculateTotal() {
-		this.precioTotal = this.precioUnitario * this.cantidad;
-	},
-};
+export const CartProvider = ({ children }) => {
+	const [cart, setCart] = useState([]);
+	const [subTotal, setSubTotal] = useState(0);
 
-const cartData = {
-	total: 0,
+	useEffect(() => {
+		setSubTotal(calcSubtotal());
+	}, [cart]);
 
-	productList: [
-		{
-			id: "1",
-			nombre: "familiar1",
-			precioTotal: 0,
+	// Cart Functions
+	const generateItem = (id, nombre, imagen, precioUnitario, detalles) => {
+		return {
+			id: id,
+			nombre: nombre,
+			imagen: imagen,
 			cantidad: 1,
-			precioUnitario: 49.9,
-			detalles: "4 royal regulares + 4 papas medianas",
-			imagen: "",
-		},
-		{
-			id: "2",
-			nombre: "Bembosboster clasica",
-			precioTotal: 0,
-			cantidad: 1,
-			precioUnitario: 12.9,
-			detalles: "1 hamburguesa broster",
-			imagen: "",
-		},
-		{
-			id: "3",
-			nombre: "Bembosboster",
-			precioTotal: 0,
-			cantidad: 1,
-			precioUnitario: 15.9,
-			detalles: "1 hamburguesa broster",
-			imagen: "",
-		},
-	],
+			precioUnitario: precioUnitario,
+			precioTotal: this.precioUnitario,
+			detalles: detalles,
+		};
+	};
 
-	calcularTotal: () => {
-		this.productList.forEach((product) => {
-			product.calculateTotal;
-			this.total += product.precioTotal;
-		});
-	},
-
-	eliminarProducto: (id) => {
-		cartData.productList = cartData.productList.filter(
-			(product) => product.id !== id
+	const addItem = (data) => {
+		const item = generateItem(
+			data.id,
+			data.nombre,
+			data.imagen,
+			data.precioUnitario,
+			data.detalles
 		);
-	},
+		calculateTotal(item);
+		setCart((prevCart) => [...prevCart, item]);
+	};
+
+	const removeItem = (id) => {
+		setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+	};
+
+	const calcSubtotal = () => cart.reduce((acc, item) => acc + item.total, 0);
+
+	// Item Functions
+	const increaseItem = (id) => {
+		setCart((prevCart) =>
+			prevCart.map((item) => {
+				return item.id === id
+					? calculateTotal({ ...item, cantidad: item.cantidad + 1 })
+					: item;
+			})
+		);
+	};
+
+	const reduceItem = (id) => {
+		setCart((prevCart) =>
+			prevCart.map((item) => {
+				return item.id === id && item.cantidad > 1
+					? calculateTotal({ ...item, cantidad: item.cantidad - 1 })
+					: item;
+			})
+		);
+	};
+
+	const calculateTotal = (item) => {
+		item.total = item.precioUnitario * item.cantidad;
+		return item;
+	};
+
+	return (
+		<CartContext.Provider
+			value={{
+				cart,
+				subTotal,
+				addItem,
+				removeItem,
+				increaseItem,
+				reduceItem,
+				calculateTotal,
+			}}
+		>
+			{children}
+		</CartContext.Provider>
+	);
 };
