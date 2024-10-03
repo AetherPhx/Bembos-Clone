f0391fb5466bd778901b0400dbdcf6c564e405f0
retosemana3 <susansharmely2@gmail.com>
2024-10-02 23:06:07 -0500

    section button cart

--- src/pages/pageButton/PageButton.jsx
+++ src/pages/pageButton/PageButton.jsx
@@ -1,1 +1,71 @@
 import "./pageButton.css";
+
+
+<div className="page-button-responsive">
+<div className="section-buttons">
+    <ul className="section-buttons-list-item">
+        <div className="section-buttons-categorias">
+            <li className="section-buttons-item-categoria">
+                Promociones Exclusivas Web
+            </li>
+        </div>
+        <div className="section-buttons-categorias">
+            <li className="section-buttons-item-categoria">
+                Combos
+            </li>
+        </div>
+        <div className="section-buttons-categorias">
+            <li className="section-buttons-item-categoria">
+                Hamburguesas
+            </li>
+       </div>
+       <div className="section-buttons-categorias">
+            <li className="section-buttons-item-categoria">
+                Pollo
+            </li>
+        </div>
+        <div className="section-buttons-categorias">
+            <li className="section-buttons-item-categoria">
+                Menus al palo
+            </li>
+       </div>
+       <div className="section-buttons-categorias">
+            <li className="section-buttons-item-categoria">
+                Loncheritas
+            </li>
+        </div>
+        <div className="section-buttons-categorias">
+            <li className="section-buttons-item-categoria">
+                Complementos
+            </li>
+       </div>
+     
+        <div className="section-buttons-categorias">
+            <li className="section-buttons-item-categoria">
+                Bebidas
+            </li>
+       </div>
+       <div className="section-buttons-categorias">
+            <li className="section-buttons-item-categoria">
+                Helados
+            </li>
+        </div>
+        <div className="section-buttons-categorias">
+            <li className="section-buttons-item-categoria">
+                Inka chips
+            </li>
+       </div>
+    </ul>
+
+</div>
+<div className="section-button-cart">
+    <div className="section-button-cart-button">
+        <img src="https://www.bembos.com.pe/_nuxt/img/carrito.76e8a3a.svg" alt="carrito"/>
+        <p>ver carrito de compras</p>
+        <span className="cart-button-span">s/ 45.20</span>
+
+    </div>
+
+</div>
+
+</div>
